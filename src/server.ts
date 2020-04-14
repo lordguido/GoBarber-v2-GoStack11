4adb48778b412d9b01125fd2009c4c98d4a5938b
Rodrigo Freitas <rodfrei@gmail.com>
2020-04-14 18:48:55 -0300

    Upgrade

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('✔ - Server started : Port[3333]...🚀');
+  console.log('✔ Server started : Port[3333]...🚀');
 });
